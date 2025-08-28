9feb4758af99ea15e1fb250ac89c91db948f4527
Mirko <163890982+mirkoWolf01@users.noreply.github.com>
2025-08-28 20:21:43 -0300

    Add files via upload

---
+++ Guia2C/ej1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+typedef struct
+{
+    char* nombre;
+    int vida;
+    double ataque;
+    double defensa;
+} monstruo_t;
+
+void print_monster(monstruo_t mon){
+    printf("Nombre: %s, Vida: %d", mon.nombre, mon.vida);
+    printf("\n");
+}
+
+
+int main()
+{
+   
+    monstruo_t monsters[SIZE] = {
+        {.nombre = "chom", .vida = 10000, .ataque = 0.2, .defensa = 130},
+        {.nombre = "elmati", .vida = 10, .ataque = 2, .defensa = 7},
+        {.nombre = "blob", .vida = 3, .ataque = 0.0001, .defensa = 0}
+    };
+
+    for(int i = 0; i < SIZE; i++){
+        print_monster(monsters[i]);
+    }
+    
+    return 0;
+}
